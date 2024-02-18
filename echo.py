460db99ba82303ba04fdd1d6ddd92864f7f452c7
Matthew Fricke <matthew@fricke.co.uk>
2024-02-17 23:23:13 -0600

    Example service for HPC class

---
+++ echo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+# Program that reads 
+import sys
+
+# Incoming Message
+message = sys.stdin.readline().strip()
+
+# Print the message
+sys.stdout.write( "Echo Server Received: " + message + "\n")
+
